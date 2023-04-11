f19c5e2bb2b0de559caeacb6519fc0802fbe85cc
Azure Dust <azurexsx@gmail.com>
2023-04-11 06:27:09 +0200

    Add project files.

---
+++ BroadCast/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BroadCast
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SimpleTcpClient client = new SimpleTcpClient();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            client.DataReceived += (sender, e) =>
+            {
+                var msg = Encoding.UTF8.GetString(e.Data);
+                if(msg.Split().Last() == "(Sport)")
+                {
+                    this.Dispatcher.Invoke(new Action(() =>
+                    {
+                        if (Sport.IsChecked == true)
+                        {
+                            TXT.Text += msg + "\n\n";
+                            return;
+                        }
+                       
+                       
+
+                    }));
+                }
+                else if(msg.Split().Last() == "(Food)")
+                {
+                    this.Dispatcher.Invoke(new Action(() => {
+                        if (Food.IsChecked == true)
+                        {
+                            TXT.Text += msg + "\n\n";
+                            return;
+                        }
+                    }));
+                }
+                else if(msg.Split().Last() == "(Furniture)")
+                {
+                    this.Dispatcher.Invoke(new Action(() => {
+                        if (Furniture.IsChecked == true)
+                        {
+                            TXT.Text += msg + "\n\n";
+                            return;
+                        }
+                    }));
+                }
+            };
+            client.Connect("127.0.0.1", 5000);
+        }
+    }
+}
---
+++ Server/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Server
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SimpleTcpServer server = new SimpleTcpServer();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            server.Start(5000);
+            Sport.IsChecked = true;
+        }
+
+        private void Button_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            this.Dispatcher.Invoke(new Action(() => {
+                if (Sport.IsChecked == true)
+                {
+                    server.Broadcast(TXT.Text + " (Sport)");
+                    return;
+                }
+                if (Food.IsChecked == true)
+                {
+                    server.Broadcast(TXT.Text + " (Food)");
+                    return;
+                }
+                if (Furniture.IsChecked == true)
+                {
+                    server.Broadcast(TXT.Text + " (Furniture)");
+                    return;
+                }
+            }));
+
+            TXT.Text = "";
+        }
+    }
+}
